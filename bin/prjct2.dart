0236ba038d30b4e49561298553386880e4e01108
niyaaspm <129578665+niyaaspm@users.noreply.github.com>
2023-08-09 13:40:12 +0530

    first commit created

---
+++ bin/prjct2.dart
@@ -0,0 +1,5 @@
+import 'package:prjct2/prjct2.dart' as prjct2;
+
+void main() {
+  print("Hello world");
+}
---
+++ test/prjct2_test.dart
@@ -0,0 +1,8 @@
+import 'package:prjct2/prjct2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
